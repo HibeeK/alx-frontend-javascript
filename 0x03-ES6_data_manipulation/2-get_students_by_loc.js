0811f728ace77894f52edad2531d099b33fff736
HibeeK <akintolauser@gmail.com>
2023-05-04 13:58:56 +0000

    fix filters

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,4 @@
-export default function getStudentsByLocation() {
+export default function getStudentsByLocation(list, city) {
+  if (!Array.isArray(list) || typeof city !== 'string') return [];
   return list.filter((value) => value.location === city);
 }
